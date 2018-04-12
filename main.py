a42ed7a3746df7ce9c289891b2028dc0b34ae991
MatthewKoch76 <matthewkoch76@gmail.com>
2018-04-11 20:16:32 -0500

    progress

--- main.py
+++ main.py
@@ -5,19 +5,54 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
-
 db = SQLAlchemy(app)
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(255))
 
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/')
+def index():
     return render_template('blogpage.html')
 
-@app.route('/addpost', methods=['POST', 'GET'])
-def addpost():
+@app.route('/blog')
+def blog():
+    
+    return render_template('blogpage.html')
 
-    return render_template('addpost.html')
+@app.route('/newpost', methods=['GET','POST'])
+def newpost():
 
+    if request.method=='POST':
+        title = request.form['title']
+        body = request.form['body']
+        title_error = ''
+        body_error = ''
+        new_post=Blog(title, body)
+
+        if title == '':
+            title_error = 'please enter a title'
+        
+        if body == '':
+            body_error = 'please enter a body'
+
+        if body_error =='':
+            if title_error =='':
+            
+                db.session.add(new_post)
+                db.session.commit()
+                post = Blog.query.all()
+                return render_template('blogpage.html', post=post)
+
+        return render_template('newpost.html', title=title, title_error=title_error, body=body, body_error=body_error)
+
+    return render_template('newpost.html')
 
 if __name__ == '__main__':
     app.run()
